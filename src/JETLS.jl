f09823f8d861c3e304c9379dbfa5e723022094f6
abap34 <abap0002@gmail.com>
2025-03-19 14:22:24 +0900

    make delayed possible at the call site

--- src/JETLS.jl
+++ src/JETLS.jl
@@ -53,9 +53,6 @@ end
 
 struct ExternalContext end
 
-const DEFAULT_DEBOUNCE_INTERVAL = 1.5
-const DEFAULT_THROTTLE_INTERVAL = 1.5
-
 struct ServerState{F}
     send::F
     workspaceFolders::Vector{URI}
@@ -307,6 +304,41 @@ function parse_file(text::String, uri::URI)
     end
 end
 
+let debounced = Dict{Any,Timer}()
+    global function debounce(f, delay, args...; kwargs...)
+        if haskey(debounced, f)
+            close(debounced[f])
+        end
+        debounced[f] = Timer(delay) do _
+            try
+                f(args...; kwargs...)
+            finally
+                delete!(debounced, f)
+            end
+        end
+        return nothing
+    end
+end
+
+let throttled = Dict{Any, Tuple{Timer, Float64}}()
+    global function throttle(f, delay, args...; kwargs...)
+        last_timer, last_time = get(throttled, f, (nothing, 0.0))
+        if last_timer !== nothing
+            close(last_timer)
+        end
+        delay = max(0.0, delay - (time() - last_time))
+        throttled[f] = Timer(delay) do _
+            try
+                f(args...; kwargs...)
+            finally
+                delete!(throttled, f)
+            end
+        end, time()
+        return nothing
+	end
+end
+
+
 function handle_DidOpenTextDocumentNotification(state::ServerState, msg::DidOpenTextDocumentNotification)
     textDocument = msg.params.textDocument
     @assert textDocument.languageId == "julia"
@@ -350,8 +382,7 @@ function handle_DidChangeTextDocumentNotification(state::ServerState, msg::DidCh
             analysis_context.result.staled = true
         end
         # TODO support multiple analysis contexts, which can happen if this file is included from multiple different contexts
-
-        debounced_reanalyze_with_context!(state, first(contexts))
+        debounce(reanalyze_with_context!, 1.5, state, first(contexts))
     end
     nothing
 end
@@ -708,42 +739,6 @@ function initiate_context!(state::ServerState, uri::URI)
 end
 
 
-function debounce(f::Function; delay::Float64=DEFAULT_DEBOUNCE_INTERVAL)
-    timer = nothing
-    function debounced_func(args...; kwargs...)
-        if timer !== nothing
-            close(timer)
-        end
-        timer = Timer(delay) do timer
-            f(args...; kwargs...)
-            timer = nothing
-        end
-    end
-    return debounced_func
-end
-
-function throttle(f::Function; delay::Float64=DEFAULT_THROTTLE_INTERVAL)
-    last_call = 0.0
-    timer = nothing
-    function throttled_func(args...; kwargs...)
-        now = time()
-        if now - last_call ≥ delay
-            f(args...; kwargs...)
-            last_call = now
-        else
-            if timer !== nothing
-                close(timer)
-            end
-            timer = Timer(delay - (now - last_call)) do timer
-                f(args...; kwargs...)
-                last_call = time()
-                timer = nothing
-            end
-        end
-    end
-    return throttled_func
-end
-
 
 function reanalyze_with_context!(state::ServerState, analysis_context::AnalysisContext)
     analysis_result = analysis_context.result
@@ -807,7 +802,5 @@ function reanalyze_with_context!(state::ServerState, analysis_context::AnalysisC
     notify_diagnostics!(state)
     nothing
 end
-
-debounced_reanalyze_with_context! = debounce(reanalyze_with_context!)
 
 end # module JETLS
