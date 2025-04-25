72470da1edee78eda3462e3bb366f7355d8120f4
Iulia Dumitru <iuliadmtru@gmail.com>
2025-04-25 15:36:14 +0300

    Move base types, messages and progress to base-protocol file

--- src/LSP/LSP.jl
+++ src/LSP/LSP.jl
@@ -5,13 +5,10 @@ using StructTypes
 const exports = Set{Symbol}()
 const method_dispatcher = Dict{String,DataType}()
 
-# Base protocol
-# =============
-
-include("base-types.jl")
 include("utils/interface.jl")
 include("utils/namespace.jl")
-include("messages.jl")
+
+include("base-protocol.jl")
 
 # Language server protocol
 # ========================
---
+++ src/LSP/base-protocol.jl
@@ -0,0 +1,253 @@
+# Base types
+# =========
+
+"""
+A special object representing `null` value.
+When used as a field specified as `StructTypes.omitempties`, the key-value pair is not
+omitted in the serialized JSON but instead appears as `null`.
+This special object is specifically intended for use in `ResponseMessage`.
+"""
+struct Null end
+const null = Null()
+StructTypes.StructType(::Type{Null}) = StructTypes.CustomStruct()
+StructTypes.lower(::Null) = nothing
+push!(exports, :Null, :null)
+
+const boolean = Bool
+const string = String
+
+"""
+Defines an integer number in the range of -2^31 to 2^31 - 1.
+"""
+const integer = Int
+
+"""
+Defines an unsigned integer number in the range of 0 to 2^31 - 1.
+"""
+const uinteger = UInt
+
+@doc """
+Defines a decimal number.
+Since decimal numbers are very rare in the language server specification we denote the exact
+range with every decimal using the mathematics interval notation (e.g. `[0, 1]` denotes all
+decimals `d` with `0 <= d <= 1`).
+"""
+const decimal = Float64
+
+@doc """
+The LSP any type
+
+# Tags
+- since – 3.17.0
+"""
+const LSPAny = Any
+
+@doc """
+LSP object definition.
+
+# Tags
+- since – 3.17.0
+"""
+const LSPObject = Dict{String,Any}
+
+@doc """
+LSP arrays.
+
+# Tags
+- since – 3.17.0
+"""
+const LSPArray = Vector{Any}
+
+# Messages
+# ========
+
+"""
+A general message as defined by JSON-RPC.
+The language server protocol always uses “2.0” as the jsonrpc version.
+"""
+@interface Message begin
+    jsonrpc::String = "2.0"
+end
+
+# Request message
+# ---------------
+
+"""
+A request message to describe a request between the client and the server.
+Every processed request must send a response back to the sender of the request.
+"""
+@interface RequestMessage @extends Message begin
+    "The request id."
+    id::Int
+
+    "The method to be invoked."
+    method::String
+
+    "The method's params."
+    params::Union{Any, Nothing} = nothing
+end
+
+# Respose message
+# ---------------
+
+@namespace ErrorCodes::Int begin
+    ParseError = -32700
+    InvalidRequest = -32600
+    MethodNotFound = -32601
+    InvalidParams = -32602
+    InternalError = -32603
+
+    """
+    This is the start range of JSON-RPC reserved error codes.
+    It doesn't denote a real error code. No LSP error codes should be defined between
+    the start and end range. For backwards compatibility the `ServerNotInitialized` and
+    the `UnknownErrorCode` are left in the range.
+
+    # Tags
+    - since – 3.16.0
+    """
+    jsonrpcReservedErrorRangeStart = -32099
+
+    """
+    # Tags
+    - deprecated – use jsonrpcReservedErrorRangeStart
+    """
+    serverErrorStart = -32099
+
+    """
+    Error code indicating that a server received a notification or request before the server
+    has received the `initialize` request.
+    """
+    ServerNotInitialized = -32002
+    UnknownErrorCode = -32001
+
+    """
+    This is the end range of JSON-RPC reserved error codes.
+    It doesn't denote a real error code.
+
+    # Tags
+    - since – 3.16.0"
+    """
+    jsonrpcReservedErrorRangeEnd = -32000
+
+    """
+    # Tags
+    - deprecated – use jsonrpcReservedErrorRangeEnd
+    """
+    serverErrorEnd = -32000
+
+    """
+    This is the start range of LSP reserved error codes.
+    It doesn't denote a real error code.
+
+    # Tags
+    - since – 3.16.0
+    """
+    lspReservedErrorRangeStart = -32899
+
+    """
+    A request failed but it was syntactically correct, e.g the method name was known and the
+    parameters were valid. The error message should contain human readable information about
+    why the request failed.
+
+    # Tags
+    - since – 3.17.0
+    """
+    RequestFailed = -32803
+
+    """
+    The server cancelled the request. This error code should only be used for requests that
+    explicitly support being server cancellable.
+
+    # Tags
+    - since – 3.17.0
+    """
+    ServerCancelled = -32802
+
+    """
+    The server detected that the content of a document got modified outside normal
+    conditions. A server should NOT send this error code if it detects a content change
+    in it unprocessed messages. The result even computed on an older state might still be
+    useful for the client. If a client decides that a result is not of any use anymore
+    the client should cancel the request.
+    """
+    ContentModified = -32801
+
+    """
+    The client has canceled a request and a server has detected the cancel.
+    """
+    RequestCancelled = -32800
+
+    """
+    This is the end range of LSP reserved error codes. It doesn't denote a real error code.
+
+    # Tags
+    - since – 3.16.0
+    """
+    lspReservedErrorRangeEnd = -32800
+end  # @namespace ErrorCodes
+
+@interface ResponseError begin
+    "A number indicating the error type that occurred."
+    code::ErrorCodes.Ty
+
+    "A string providing a short description of the error."
+    message::String
+
+    """
+    A primitive or structured value that contains additional information about the error.
+    Can be omitted.
+    """
+    data::Union{Any, Nothing} = nothing
+end
+
+# TODO Revisit this to correctly lower this struct
+"""
+A Response Message sent as a result of a request.
+If a request doesn’t provide a result value the receiver of a request still needs to return
+a response message to conform to the JSON-RPC specification.
+The result property of the ResponseMessage should be set to null in this case to signal a
+successful request.
+"""
+@interface ResponseMessage @extends Message begin
+    "The request id."
+    id::Union{Int, Nothing}
+
+    """
+    The result of a request. This member is REQUIRED on success.
+    This member MUST NOT exist if there was an error invoking the method.
+    """
+    result::Union{Any, Nothing} = nothing
+
+    "The error object in case a request fails."
+    error::Union{ResponseError, Nothing} = nothing
+end
+
+# Notification message
+# --------------------
+
+"""
+A notification message. A processed notification message must not send a response back.
+They work like events.
+"""
+@interface NotificationMessage @extends Message begin
+    "The method to be invoked."
+    method::String
+
+    "The notification's params."
+    params::Union{Any, Nothing} = nothing
+end
+
+# Cancellation support
+# ====================
+
+# Progress support
+# ================
+
+"""
+The base protocol offers also support to report progress in a generic fashion.
+This mechanism can be used to report any kind of progress including work done progress
+(usually used to report progress in the user interface using a progress bar) and partial
+result progress to support streaming of results.
+"""
+const ProgressToken = Union{Int, String}
