0c18c6543739eeea7378d433b30d3c29a198e9ae
Iulia Dumitru <iuliadmtru@gmail.com>
2025-04-25 17:15:08 +0300

    Combine pieces

--- src/LSP/LSP.jl
+++ src/LSP/LSP.jl
@@ -10,12 +10,15 @@ include("utils/namespace.jl")
 
 include("base-protocol.jl")
 include("basic-json-structures.jl")
-include("capabilities/server.jl")
-include("capabilities/client.jl")
-include("lifecycle-messages.jl")
-
-include("documents.jl")
-include("diagnostics.jl")
+include("lifecycle-messages/register-capability.jl")
+include("document-synchronization.jl")
+include("language-features/diagnostics.jl")
+include("workspace-features/workspace-folders.jl")
+include("workspace-features/files.jl")
+include("capabilities.jl")
+include("lifecycle-messages/initialize.jl")
+include("lifecycle-messages/shutdown.jl")
+include("lifecycle-messages/exit.jl")
 
 for name in exports
     Core.eval(@__MODULE__, Expr(:export, name))
