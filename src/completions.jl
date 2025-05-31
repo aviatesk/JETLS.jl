559c6d77752e08e009a960291b9badf3926ead4f
abap34 <abap0002@gmail.com>
2025-05-31 23:34:29 +0900

    Skip analysis for macros when the program is empty

--- src/completions.jl
+++ src/completions.jl
@@ -398,8 +398,10 @@ function global_completions!(items::Dict{String, CompletionItem}, state::ServerS
         # check if the cursor is within the macro name context `@xxx|`
         curbyte = xy_to_offset(fi, pos)
         sn = JS.build_tree(JS.SyntaxNode, fi.parsed_stream)
-        curnode = first(byte_ancestors(sn, curbyte-1))
-        is_macro_invoke = JS.kind(curnode) === K"MacroName"
+        if !isempty(sn.children) 
+            curnode = first(byte_ancestors(sn, curbyte-1))
+            is_macro_invoke = JS.kind(curnode) === K"MacroName"
+        end
     end
     for name in @invokelatest(names(mod; all=true, imported=true, usings=true))::Vector{Symbol}
         s = String(name)
