d140bf943557481d0fdc0d9e59dd75c2f81d3eb8
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2025-06-15 04:07:37 +0900

    completion: special case `@nospecialize` (#86)
    `@nospecialize` is very common in our code base and ignoring it
    completely
    makes us unable to get completions for any methods using it,
    since it makes method signatures completely broken.
    
    Fortunately JL can handle it (probably because JL special-cases it),
    so we can just keep it and support completions for such methods.

--- src/completions.jl
+++ src/completions.jl
@@ -113,7 +113,12 @@ function remove_macrocalls(st0::JL.SyntaxTree)
     ctx = JL.MacroExpansionContext(JL.syntax_graph(st0), JL.Bindings(),
                                    JL.ScopeLayer[], JL.ScopeLayer(1, Module(), false))
     if kind(st0) === K"macrocall"
-        @ast ctx st0 "nothing"::K"core"
+        macroname = st0[1]
+        if hasproperty(macroname, :name_val) && macroname.name_val == "@nospecialize"
+            st0
+        else
+            @ast ctx st0 "nothing"::K"core"
+        end
     elseif JS.is_leaf(st0)
         st0
     else
@@ -147,7 +152,7 @@ function cursor_bindings(st0_top::JL.SyntaxTree, b_top::Int)
     ctx3, st2 = try
         jl_lower_for_completion(st0)
     catch err
-        # @info "Error in lowering" err
+        JETLS_DEV_MODE && @warn "Error in lowering" err
         return nothing # lowering failed, e.g. because of incomplete input
     end
 
