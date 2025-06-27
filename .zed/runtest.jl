4a842bfe301994599833b61213c6867b106f3895
Shuhei Kadowaki <aviatesk@gmail.com>
2025-06-27 18:34:51 +0900

    zed: add runtest task

---
+++ .zed/runtest.jl
@@ -0,0 +1,9 @@
+using Test
+
+function @main(args)
+    if length(args) < 2
+        error("Usage: runtest.jl <testset_name> <testfile>")
+    end
+    @testset "$(args[1])" include(args[2])
+    return 0
+end
