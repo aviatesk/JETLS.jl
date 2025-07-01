55340e2a14ae0f18de0d575cb656757f8f2f7560
Shuhei Kadowaki <aviatesk@gmail.com>
2025-07-01 22:18:47 +0900

    update Zed editor tasks to use TestRunner.jl integration

--- .zed/runtest.jl
+++
@@ -1,9 +0,0 @@
-using Test
-
-function (@main)(args)
-    if length(args) < 2
-        error("Usage: runtest.jl <testset_name> <testfile>")
-    end
-    @testset "$(args[1])" include(args[2])
-    return 0
-end
