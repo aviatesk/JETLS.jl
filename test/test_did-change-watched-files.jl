a0a4d3aa2e01da1077077d21c644139c085220f9
abap34 <abap0002@gmail.com>
2025-07-30 23:56:26 +0900

    simplify file writing
    Co-Authored-By: Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>

--- test/test_did-change-watched-files.jl
+++ test/test_did-change-watched-files.jl
@@ -37,14 +37,12 @@ const TESTRUNNER_DEFAULT = JETLS.access_nested_dict(JETLS.DEFAULT_CONFIG,
         config_path = joinpath(tmpdir, ".JETLSConfig.toml")
         DEBOUNCE_STARTUP = 100.0
         TESTRUNNER_STARTUP = "testrunner_startup"
-        open(config_path, "w") do io
-            write(io, """
-                [performance.full_analysis]
-                debounce = $DEBOUNCE_STARTUP
-                [testrunner]
-                executable = \"$TESTRUNNER_STARTUP\"
-                """)
-        end
+        write(config_path, """
+            [performance.full_analysis]
+            debounce = $DEBOUNCE_STARTUP
+            [testrunner]
+            executable = \"$TESTRUNNER_STARTUP\"
+            """)
         rootUri = filepath2uri(tmpdir)
         withserver(; rootUri, capabilities=CLIENT_CAPABILITIES) do (; writereadmsg, readmsg, id_counter, server)
             manager = server.state.config_manager
@@ -67,14 +65,12 @@ const TESTRUNNER_DEFAULT = JETLS.access_nested_dict(JETLS.DEFAULT_CONFIG,
 
             # change `performance.full_analysis.debounce` to `DEBOUNCE_V2`
             DEBOUNCE_V2 = 200.0
-            open(config_path, "w") do io
-                write(io, """
-                    [performance.full_analysis]
-                    debounce = $DEBOUNCE_V2
-                    [testrunner]
-                    executable = \"$TESTRUNNER_STARTUP\"
-                    """)
-            end
+            write(config_path, """
+                [performance.full_analysis]
+                debounce = $DEBOUNCE_V2
+                [testrunner]
+                executable = \"$TESTRUNNER_STARTUP\"
+                """)
 
             change_notification = DidChangeWatchedFilesNotification(;
                 params=DidChangeWatchedFilesParams(;
@@ -96,15 +92,13 @@ const TESTRUNNER_DEFAULT = JETLS.access_nested_dict(JETLS.DEFAULT_CONFIG,
 
             THROTTLE_V2 = 300.0
             # Add a new key `performance.full_analysis.throttle`
-            open(config_path, "w") do io
-                write(io, """
-                    [performance.full_analysis]
-                    debounce = $DEBOUNCE_V2
-                    throttle = $THROTTLE_V2
-                    [testrunner]
-                    executable = \"$TESTRUNNER_STARTUP\"
-                    """)
-            end
+            write(config_path, """
+                [performance.full_analysis]
+                debounce = $DEBOUNCE_V2
+                throttle = $THROTTLE_V2
+                [testrunner]
+                executable = \"$TESTRUNNER_STARTUP\"
+                """)
 
             change_notification1 = DidChangeWatchedFilesNotification(;
                 params=DidChangeWatchedFilesParams(;
@@ -125,14 +119,12 @@ const TESTRUNNER_DEFAULT = JETLS.access_nested_dict(JETLS.DEFAULT_CONFIG,
 
             # Change `testrunner.executable` to "newtestrunner"
             TESTRUNNER_V2 = "testrunner_v2"
-            open(config_path, "w") do io
-                write(io, """
-                    [performance.full_analysis]
-                    debounce = $DEBOUNCE_V2
-                    [testrunner]
-                    executable = \"$TESTRUNNER_V2\"
-                    """)
-            end
+            write(config_path, """
+                [performance.full_analysis]
+                debounce = $DEBOUNCE_V2
+                [testrunner]
+                executable = \"$TESTRUNNER_V2\"
+                """)
 
             change_notification2 = DidChangeWatchedFilesNotification(;
                 params=DidChangeWatchedFilesParams(;
@@ -146,12 +138,10 @@ const TESTRUNNER_DEFAULT = JETLS.access_nested_dict(JETLS.DEFAULT_CONFIG,
             @test JETLS.get_config(manager, "testrunner", "executable") == TESTRUNNER_V2
 
             # unknown keys should be reported
-            open(config_path, "w") do io
-                write(io, """
-                    [performance]
-                    ___unknown_key___ = \"value\"
-                    """)
-            end
+            write(config_path, """
+                [performance]
+                ___unknown_key___ = \"value\"
+                """)
 
             change_notification3 = DidChangeWatchedFilesNotification(;
                 params=DidChangeWatchedFilesParams(;
@@ -192,14 +182,12 @@ const TESTRUNNER_DEFAULT = JETLS.access_nested_dict(JETLS.DEFAULT_CONFIG,
             # re-create the config file
             DEBOUNCE_RECREATE = 400.0
             TESTRUNNER_RECREATE = "testrunner_recreate"
-            open(config_path, "w") do io
-                write(io, """
-                    [performance.full_analysis]
-                    debounce = $DEBOUNCE_RECREATE
-                    [testrunner]
-                    executable = \"$TESTRUNNER_RECREATE\"
-                    """)
-            end
+            write(config_path, """
+                [performance.full_analysis]
+                debounce = $DEBOUNCE_RECREATE
+                [testrunner]
+                executable = \"$TESTRUNNER_RECREATE\"
+                """)
 
             re_creation_notification = DidChangeWatchedFilesNotification(;
                 params=DidChangeWatchedFilesParams(;
@@ -251,14 +239,12 @@ end
             config_path = joinpath(tmpdir, ".JETLSConfig.toml")
             DEBOUNCE_RECREATE = 500.0
             TESTRUNNER_RECREATE = "testrunner_recreate"
-            open(config_path, "w") do io
-                write(io, """
-                    [performance.full_analysis]
-                    debounce = $DEBOUNCE_RECREATE
-                    [testrunner]
-                    executable = \"$TESTRUNNER_RECREATE\"
-                    """)
-            end
+            write(config_path, """
+                [performance.full_analysis]
+                debounce = $DEBOUNCE_RECREATE
+                [testrunner]
+                executable = \"$TESTRUNNER_RECREATE\"
+                """)
             creation_notification = DidChangeWatchedFilesNotification(;
                 params=DidChangeWatchedFilesParams(;
                     changes=[FileEvent(; uri=filepath2uri(config_path), type=FileChangeType.Created)]
@@ -281,14 +267,12 @@ end
             # New config file change also should be watched
             DEBOUNCE_V2 = 600.0
             TESTRUNNER_V2 = "testrunner_v2"
-            open(config_path, "w") do io
-                write(io, """
-                    [performance.full_analysis]
-                    debounce = $DEBOUNCE_V2
-                    [testrunner]
-                    executable = \"$TESTRUNNER_V2\"
-                    """)
-            end
+            write(config_path, """
+                [performance.full_analysis]
+                debounce = $DEBOUNCE_V2
+                [testrunner]
+                executable = \"$TESTRUNNER_V2\"
+                """)
             change_notification = DidChangeWatchedFilesNotification(;
                 params=DidChangeWatchedFilesParams(;
                     changes=[FileEvent(; uri=filepath2uri(config_path), type=FileChangeType.Changed)]
