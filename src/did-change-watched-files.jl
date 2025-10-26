ccbb696a6413f56a5fb842d8aaacc5eeb289f646
Shuhei Kadowaki <aviatesk@gmail.com>
2025-10-27 04:53:48 +0900

    cosmetic code movement

--- src/did-change-watched-files.jl
+++ src/did-change-watched-files.jl
@@ -44,6 +44,68 @@ function handle_config_file_change!(
     notify_config_changes(server, tracker, source)
 end
 
+"""
+Loads the project configuration from the specified path into the server's config manager.
+
+If the file does not exist or cannot be parsed, just return leaving the current
+configuration unchanged. When there are unknown keys in the config file,
+send error message while leaving current configuration unchanged.
+"""
+function load_config!(callback, server::Server, filepath::AbstractString;
+                      reload::Bool = false)
+    store!(server.state.config_manager) do old_data::ConfigManagerData
+        if reload && old_data.project_config_path != filepath
+            show_warning_message(server, "Loading unregistered configuration file: $filepath")
+        end
+
+        isfile(filepath) || return old_data, nothing
+        parsed = TOML.tryparsefile(filepath)
+        parsed isa TOML.ParserError && return old_data, nothing
+
+        new_project_config = try
+            Configurations.from_dict(JETLSConfig, parsed)
+        catch e
+            # TODO: remove this when Configurations.jl support to report
+            #       full path of unknown key.
+            if e isa Configurations.InvalidKeyError
+                config_dict = to_config_dict(parsed)
+                unknown_keys = collect_unmatched_keys(config_dict)
+                if !isempty(unknown_keys)
+                    show_error_message(server, unmatched_keys_in_config_file_msg(filepath, unknown_keys))
+                    return old_data, nothing
+                end
+            end
+            show_error_message(server, """
+                Failed to load configuration file at $filepath:
+                $(e)
+                """)
+            return old_data, nothing
+        end
+
+        new_data = ConfigManagerData(old_data;
+            project_config=new_project_config,
+            project_config_path=filepath
+        )
+        on_difference(callback, get_settings(old_data), get_settings(new_data))
+        return new_data, nothing
+    end
+end
+
+unmatched_keys_in_config_file_msg(filepath::AbstractString, unmatched_keys) =
+    unmatched_keys_msg("Configuration file at $filepath contains unknown keys:", unmatched_keys)
+
+function delete_config!(callback, manager::ConfigManager, filepath::AbstractString)
+    store!(manager) do old_data::ConfigManagerData
+        old_data.project_config_path == filepath || return old_data, nothing
+        new_data = ConfigManagerData(old_data;
+            project_config=EMPTY_CONFIG,
+            project_config_path=nothing
+        )
+        on_difference(callback, get_settings(old_data), get_settings(new_data))
+        return new_data, nothing
+    end
+end
+
 function handle_DidChangeWatchedFilesNotification(server::Server, msg::DidChangeWatchedFilesNotification)
     for change in msg.params.changes
         changed_path = uri2filepath(change.uri)
