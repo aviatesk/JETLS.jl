91edce8f60513c5a11929f693c2d74aec8ed3ad4
Shuhei Kadowaki <aviatesk@gmail.com>
2025-12-20 02:12:16 +0900

    Tweak `jldebug` to return context and syntax tree

--- test/jsjl-utils.jl
+++ test/jsjl-utils.jl
@@ -35,18 +35,18 @@ end
 # dump all intermediate ctx and st into the global scope for inspection
 # use `stop` if some lowering pass mutates ctx in a way you don't want
 function jldebug(mod::Module, st0_in::JL.SyntaxTree, stop::Int=5)
-    global ctx5, st5, ctx4, st4, ctx3, st3, ctx2, st2, ctx1, st1
-    stop = stop - 1; stop < 0 && return; ctx1, st1 = JL.expand_forms_1(mod, st0_in, true, Base.get_world_counter())
-    stop = stop - 1; stop < 0 && return; ctx2, st2 = JL.expand_forms_2(ctx1, st1)
-    stop = stop - 1; stop < 0 && return; ctx3, st3 = JL.resolve_scopes(ctx2, st2)
-    stop = stop - 1; stop < 0 && return; ctx4, st4 = JL.convert_closures(ctx3, st3)
-    stop = stop - 1; stop < 0 && return; ctx5, st5 = JL.linearize_ir(ctx4, st4)
-    st5
+    global st0 = st0_in
+    global st1, st2, st3, st4, st5
+    global ctx1, ctx2, ctx3, ctx4, ctx5
+    ctx0 = nothing
+    (stop -= 1) < 0 && return ctx0, st0; ctx1, st1 = JL.expand_forms_1(mod, st0_in, true, Base.get_world_counter())
+    (stop -= 1) < 0 && return ctx1, st1; ctx2, st2 = JL.expand_forms_2(ctx1, st1)
+    (stop -= 1) < 0 && return ctx2, st2; ctx3, st3 = JL.resolve_scopes(ctx2, st2)
+    (stop -= 1) < 0 && return ctx3, st3; ctx4, st4 = JL.convert_closures(ctx3, st3)
+    (stop -= 1) < 0 && return ctx4, st4; ctx5, st5 = JL.linearize_ir(ctx4, st4)
+    return ctx5, st5
 end
-function jldebug(mod::Module, s::AbstractString, stop::Int=5)
-    global st0 = jlparse(s; rule=:statement)
-    jldebug(mod, st0, stop)
-end
+jldebug(mod::Module, s::AbstractString, stop::Int=5) = jldebug(mod, jlparse(s; rule=:statement), stop)
 jldebug(args...) = jldebug(@__MODULE__, args...)
 
 # Select a node by ID from a tree (its underlying graph), graph, or ctx
