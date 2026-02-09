1862ce898f1db62d0589c4f688f76b93b1a2fa01
abap34 <abap0002@gmail.com>
2026-02-09 21:55:30 +0900

    check JSONSchema instead of generation in CI

--- scripts/schema/generate.jl
+++ scripts/schema/generate.jl
@@ -6,7 +6,17 @@ setup_ctx!(gen_ctx)
 schema = generate_schema(JETLS.JETLSConfig; ctx = gen_ctx)
 
 schemafile_path = joinpath(@__DIR__, "..", "..", "jetls-config.schema.json")
-open(schemafile_path, "w") do io
-    write(io, JSON.json(schema.doc, 2))
+expected = JSON.json(sort_keys(schema.doc), 2)
+
+if "--check" in ARGS
+    if read(schemafile_path, String) != expected
+        @warn "jetls-config.schema.json does not match the expected schema. Please run this script without --check to update it."
+        exit(1)
+    end
+else
+    open(schemafile_path, "w") do io
+        write(io, expected)
+    end
+    @info "Updated jetls-config.schema.json with the new schema."
 end
 
