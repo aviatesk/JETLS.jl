f199302906e9ba4db8fad4b1e3d4bab9486352ad
Shuhei Kadowaki <40514306+aviatesk@users.noreply.github.com>
2026-04-17 00:25:03 +0900

    Update src/diagnostic.jl

--- src/diagnostic.jl
+++ src/diagnostic.jl
@@ -258,7 +258,7 @@ end
 function apply_markdown_message!(diagnostics::Vector{Diagnostic})
     for i = 1:length(diagnostics)
         diagnostic = diagnostics[i]
-        if diagnostic.message isa String && occursin('`', diagnostic.message)
+        if diagnostic.message isa String
             diagnostics[i] = Diagnostic(diagnostic; message = MarkupContent(; kind = MarkupKind.Markdown, value = diagnostic.message))
         end
     end
