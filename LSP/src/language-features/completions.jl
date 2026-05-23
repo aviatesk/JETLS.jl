bb8f40e4ca877bb41b5c36e7906061be8eddc41d
Shuhei Kadowaki <aviatesk@gmail.com>
2026-05-23 22:54:56 +0900

    LSP: Fix doc comment formatting in completions

--- LSP/src/language-features/completions.jl
+++ LSP/src/language-features/completions.jl
@@ -59,13 +59,16 @@ end
 
 "How a completion was triggered"
 @namespace CompletionTriggerKind::Int begin
-    "Completion was triggered by typing an identifier (24x7 code
-    complete), manual invocation (e.g Ctrl+Space) or via API."
+    """
+    Completion was triggered by typing an identifier (24x7 code complete),
+    manual invocation (e.g Ctrl+Space) or via API.
+    """
     Invoked = 1
 
-    "Completion was triggered by a trigger character specified by
-    the `triggerCharacters` properties of the
-    `CompletionRegistrationOptions`."
+    """
+    Completion was triggered by a trigger character specified by the `triggerCharacters`
+    properties of the `CompletionRegistrationOptions`.
+    """
     TriggerCharacter = 2
 
     "Completion was re-triggered as the current completion list is incomplete."
@@ -80,9 +83,10 @@ request is triggered.
     "How the completion was triggered."
     triggerKind::CompletionTriggerKind.Ty
 
-    "The trigger character (a single character) that has trigger code
-    complete. Is undefined if
-    `triggerKind !== CompletionTriggerKind.TriggerCharacter`"
+    """
+    The trigger character (a single character) that has trigger code complete.
+    Is undefined if `triggerKind !== CompletionTriggerKind.TriggerCharacter`
+    """
     triggerCharacter::Union{String, Nothing} = nothing
 end
 
